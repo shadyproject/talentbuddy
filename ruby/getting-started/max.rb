f294a777157431dba8de0edba69781309a33e6a6
unknown <shady@shadyproject.net>
2014-06-22 00:16:04 -0700

    Getting Started code

---
+++ ruby/getting-started/max.rb
@@ -0,0 +1,5 @@
+#task: write a function that prints to stdout the maximum value between two arguments
+def get_max(a, b)
+  puts [a,b].max
+end
+
---
+++ ruby/getting-started/simple-sum.rb
@@ -0,0 +1,5 @@
+#task: write a function that prints to stdout their sum
+
+def get_sum(a, b)
+  puts (a+b)
+end
